074272d05735f09d5caf81f1659b130a05174292
Jay2250 <jayeshchauriwar@gmail.com>
2024-07-14 17:40:18 +0530

    staticfiles_build folder added

--- PortFolio/urls.py
+++ PortFolio/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from PortFolio import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Base.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
