3e55d748d84290cbe9e855df4133fd92606c10ad
Jay2250 <jayeshchauriwar@gmail.com>
2024-07-14 17:24:54 +0530

    staticfiles_build folder added

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
 python3.12 -m install -r requirements.txt
+python3.12 manage.py makemigratioins
+python3.12 manage.py migrate
 python3.12 manage.py collectstatic 
 echo "BUILD END"
